af447eacf1d5298abcd4fd05ca9998c39e026bc8
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-13 15:48:58 +0200

    Potrzebne .section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TopBar} from './sections'
+import {TopBar, Attention,Team, Offer,Footer} from './sections'
 import './App.css';
 
 
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="app">
       <TopBar />
+      <Attention />
+      <Team/>
+      <Offer/>
+      <Footer/>
     </div>
   );
 }
--- src/sections.js
+++ src/sections.js
@@ -1,23 +1,30 @@
 import React from 'react';
 
 // deklaracja top bar jest funkcją i {i wywołuje komponent}
-const TopBar = () =>{
+// deklaracje
+const TopBar =() => {
   return (<h1>TopBar</h1>)
+
 }
+const Attention = () => {
+  return (<h1>Attention</h1>)
+}
+const Team = ()=> {
+  return (<h1> Team</h1>)
+}
+
+const Offer = ()=> {
+  return (<h1> Offer</h1>)
+}
+
+const Footer = ()=> {
+  return (<h1>footer </h1>)
+}
+
 
 export {
-  TopBar
+  TopBar, Attention, Team, Offer,Footer
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
