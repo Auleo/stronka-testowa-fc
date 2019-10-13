42614da26571770825aa3d932aed0aa8bda0377d
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-13 09:45:48 +0200

    Start sections

---
+++ src/sections.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// deklaracja top bar jest funkcją i {i wywołuje komponent}
+const TopBar = () =>{
+  return (<h1>TopBar</h1>)
+}
+
+export {
+  TopBar
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// nazwa sections wskazuje ,że plików jest wicej i nie da sie zrobić
+// export deafault dlatego dla wielu PLIKÓW plan mówi wielu stosujemy export {co ś tam TopBar}
