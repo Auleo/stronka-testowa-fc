26b4f38bd07699cca8f624030bcc17ef63eafb95
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-27 22:58:49 +0100

    before  yarn build

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-//import { Jumbotron, Button } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 
 const Landing = () => {
-  return (<h1>Landing</h1>)
+  return (
+    <div>
+    { }
+  <h1>Nasza firma oferuje najwyższej jakosci produkty.</h1>
+  <h3>Nie wierz nam na słowo - sprawdź</h3>
+  <Button variant="outline-success">Oferta</Button>
+  </div>
+  )
 
 }
 export default Landing;
